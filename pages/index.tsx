1332277dddb7d2f6a98a8edc3e9166274f86a7b2
Hayden Buckley-Smith <hayden@abstack.co>
2022-12-23 09:24:27 +0000

    feat: Email Subscribe card with styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <div className='grid grid-cols-3'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+      </div>
+    </div>
+  );
 }
