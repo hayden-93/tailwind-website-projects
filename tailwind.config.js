b0968807994ce09703a0967eb1eb2eae5cc6b192
Hayden Buckley-Smith <hayden@abstack.co>
2022-12-23 08:47:57 +0000

    chore: install react-feather icon library and add custom screen tokens to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+    },
     extend: {},
   },
   plugins: [],
