90bdf034e88cb182b6fcf934a202a7e648bf527f
Hayden Buckley-Smith <hayden@abstack.co>
2022-12-30 11:42:13 +0000

    feat: add link to Image Gallery project to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,21 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <div className='grid grid-cols-3'>
+    <div className='max-w-screen-md mx-auto mt-10'>
+      <div className='grid grid-cols-3 gap-2'>
         <Link href='/email-subscribe-card'>
           <Image
             src='/images/email-subscribe-card/emailsubscribe.png'
             alt='Email Card Image'
+            width={300}
+            height={300}
+            className='rounded h-full'
+          />
+        </Link>
+        <Link href='/image-gallery'>
+          <Image
+            src='/images/image-gallery/image-gallery.png'
+            alt='Image Gallery'
             width={300}
             height={300}
             className='rounded'
