0d11c50057f048226796fb88051dec3c51b44fa1
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-05 15:15:22 +0000

    feat: Pricing Grid

---
+++ pages/pricing-grid.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Card } from "../src/components/pricing-grid/Card";
+
+const PricingGrids = () => {
+  return (
+    <div className="min-h-screen bg-slate-800">
+      <div className="max-w-screen-md mx-auto">
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+export default PricingGrids;
