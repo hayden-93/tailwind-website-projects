2c87588c6381225773d9f0d3814286457acba3b1
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-09 08:53:44 +0000

    feat: Product modal button group
    add button group to product details

--- pages/product-modal.tsx
+++ pages/product-modal.tsx
@@ -31,6 +31,39 @@ const ProductModal = () => {
               This offer is valid until April 3rd or as long as stock lasts!
             </p>
           </div>
+          <div className='group'>
+            <button className='w-full transition-all duration-150 bg-blue-700 text-white border-b-8 border-b-blue-700 rounded-lg group-hover:border-t-8 group-hover:border-b-0 group-hover:bg-blue-700 group-hover:border-t-blue-700 group-hover:shadow-lg'>
+              <span className='block px-8 py-4 duration-150 bg-blue-500 rounded-lg group-hover:bg-blue-700'>
+                Add to cart
+              </span>
+            </button>
+          </div>
+          <div className='flex items-center space-x-3 group'>
+            <span className='w-3 h-3 bg-green-400 rounded-full group-hover:animate-ping'></span>
+            <span className='text-xs'>50+ pcs. in stock</span>
+          </div>
+          <div className='flex space-x-2 items-baseline'>
+            <button className='flex justify-center items-center py-1 px-3 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150'>
+              <Image
+                src='/images/product-modal/weight.png'
+                alt='Scales image'
+                width={24}
+                height={24}
+              />
+              <span className='text-xs'>Add to cart</span>
+            </button>
+            <div>
+              <button className='flex justify-center items-center py-1 px-3 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150'>
+                <Image
+                  src='/images/product-modal/heart.png'
+                  alt='Heart image'
+                  width={24}
+                  height={24}
+                />
+                <span className='text-xs'>Add to wishlist</span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
