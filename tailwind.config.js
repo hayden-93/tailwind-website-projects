7de9cfdb9490a6b6212322cf01920bfe9011cec3
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-10 15:05:32 +0000

    chore: add custom tokens to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
+      },
+      colors: {
+        primary: {},
+        neutral: {},
+      },
+      fontFamily: {
+        body: ['Bai Jamjuree'],
+      },
+    },
   },
   plugins: [],
 };
