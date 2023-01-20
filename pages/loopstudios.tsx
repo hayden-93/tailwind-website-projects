7bda4b89729f2df8c7bbc19771ba76431ff34a80
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-20 09:59:07 +0000

    feat: interactive VR feature section

--- pages/loopstudios.tsx
+++ pages/loopstudios.tsx
@@ -62,6 +62,27 @@ const LoopStudios = () => {
           Impressive experiences that deliver
         </p>
       </section>
+      <section id='feature'>
+        <div className='relative container flex flex-col max-w-screen-loop-xl mx-auto my-32 px-6 text-gray-900 md:flex-row md:px-0'>
+          <Image
+            src='/images/loopstudios/image-interactive.jpg'
+            alt='Man playing on VR headset'
+            width={731}
+            height={500}
+          />
+          <div className='top-48 pr-0 bg-white md:absolute md:right-0 md:py-20 md:pl-20'>
+            <h2 className='max-w-screen-loop-lg mt-10 mb-6 font-sans text-4xl text-gray-900 uppercase text-center md:text-5xl md:mt-0 md:text-left'>
+              The leader in interactive VR
+            </h2>
+            <p className='max-w-md text-center md:text-left'>
+              Founded in 2011, Loopstudios has been producing world-class
+              virtual reality projects for some of the best companies around the
+              globe. Our award-winning creations have transformed businesses
+              through digital experiences that bind to their brand.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
