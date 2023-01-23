834c70b2e139e7a32b0738c54d1386aa39a6b652
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-23 15:42:03 +0000

    chore: add custom tokens to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'shortly-cyan': 'hsl(180, 66%, 49%)',
+        'shortly-cyanLight': 'hsl(180, 66%, 69%)',
+        'shortly-darkViolet': 'hsl(257, 27%, 26%)',
+        'shortly-red': 'hsl(0, 87%, 67%)',
+        'shortly-grayishViolet': 'hsl(257, 7%, 63%)',
+        'shortly-veryDarkBlue': 'hsl(255, 11%, 22%)',
+        'shortly-veryDarkViolet': 'hsl(260, 8%, 14%) ',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
+      },
+    },
   },
   plugins: [],
 };
