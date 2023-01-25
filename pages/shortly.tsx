dd455ad04e9a316b9799574d4792563b240167e5
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-25 14:50:52 +0000

    feat: add navbar with logo and links

--- pages/shortly.tsx
+++ pages/shortly.tsx
@@ -1,8 +1,62 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Shortly = () => {
   return (
     <div>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <header>
+        <nav className='relative container mx-auto p-6'>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center space-x-20'>
+              <Image
+                src='/images/shortly/logo.svg'
+                alt=''
+                width={121}
+                height={33}
+              />
+              <div className='hidden space-x-8 font-bold lg:flex'>
+                <Link
+                  href='#'
+                  className='text-shortly-grayishViolet hover:text-shortly-veryDarkViolet'
+                >
+                  Features
+                </Link>
+                <Link
+                  href='#'
+                  className='text-shortly-grayishViolet hover:text-shortly-veryDarkViolet'
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href='#'
+                  className='text-shortly-grayishViolet hover:text-shortly-veryDarkViolet'
+                >
+                  Resources
+                </Link>
+              </div>
+            </div>
+            <div className='hidden items-center space-x-6 font-bold text-shortly-grayishViolet lg:flex'>
+              <div className='hover:text-shortly-veryDarkViolet'>Login</div>
+              <Link
+                href='#'
+                className='py-3 px-8 font-bold text-white bg-shortly-cyan rounded-full hover:opacity-70'
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
       <div></div>
     </div>
   );
