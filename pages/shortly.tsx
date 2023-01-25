36053b2b99cb7455780ca089f6c09d9b880fd742
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-25 14:51:40 +0000

    feat: hero section

--- pages/shortly.tsx
+++ pages/shortly.tsx
@@ -56,6 +56,36 @@ const Shortly = () => {
             </div>
           </div>
         </nav>
+        <section id='hero' className=''>
+          <div className='container flex flex-col-reverse mx-auto p-6 lg:flex-row'>
+            <div className='flex flex-col space-y-10 mb-44 lg:mt-16 lg:w-1/2 xl:mb-52'>
+              <h1 className='text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left'>
+                More than shorter links
+              </h1>
+              <p className='text-2xl text-center text-gray-400 lg:max-w-md lg:text-left'>
+                build your brands recognition and get detailed insights on how
+                your links are performing.
+              </p>
+              <div className='mx-auto lg:mx-0'>
+                <Link
+                  href='#'
+                  className='py-5 px-10 text-2xl font-bold text-white bg-shortly-cyan rounded-full lg:py-4 hover:opacity-70'
+                >
+                  Get Started
+                </Link>
+              </div>
+            </div>
+            <div className='mb-24 mx-auto md:w-180 lg:mb-0 lg:w-1/2'>
+              <Image
+                src='/images/shortly/illustration-working.svg'
+                alt='Hero image - person sat working'
+                width={733}
+                height={482}
+                className=''
+              />
+            </div>
+          </div>
+        </section>
       </header>
       <div></div>
     </div>
