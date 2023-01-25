8b17d6036548031ff0da2bfa72ba7f2dda19f734
Hayden Buckley-Smith <hayden@abstack.co>
2023-01-25 15:59:16 +0000

    feat: add shorter url links section

--- pages/shortly.tsx
+++ pages/shortly.tsx
@@ -57,7 +57,7 @@ const Shortly = () => {
           </div>
         </nav>
       </header>
-      <section className="">
+      <section>
         <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row">
           <div className="flex flex-col space-y-10 mb-44 lg:mt-16 lg:w-1/2 xl:mb-52">
             <h1 className="text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left">
@@ -87,7 +87,70 @@ const Shortly = () => {
           </div>
         </div>
       </section>
-      <div></div>
+      <section id="shorten" className="relative bg-gray-100">
+        <div className="max-w-4xl mx-auto p-6 space-y-6">
+          <form
+            id="link-form"
+            className="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-shortly-darkViolet rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+          >
+            <input
+              type="text"
+              className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
+              placeholder="Shorten a link here"
+              id="link-input"
+            />
+
+            <button className="px-10 py-3 text-white bg-shortly-cyan rounded-lg hover:bg-shortly-cyanLight focus:outline-none md:py-2">
+              Shorten It!
+            </button>
+            <div
+              id="err-msg"
+              className="absolute left-7 bottom-3 text-red text-sm italic"
+            ></div>
+          </form>
+          <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
+            <p className="font-bold text-center text-shortly-veryDarkViolet md:text-left">
+              https://frontendmentor.io
+            </p>
+            <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+              <div className="font-bold text-shortly-cyan">
+                https://rel.ink/k4IKyk
+              </div>
+              <button className="p-2 px-8 text-white bg-shortly-cyan rounded-lg hover:opacity-70 focus:outline-none">
+                Copy
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
+            <p className="font-bold text-center text-shortly-veryDarkViolet md:text-left">
+              https://twitter.com/frontendmentor
+            </p>
+
+            <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+              <div className="font-bold text-shortly-cyan">
+                https://rel.ink/gxOXp9
+              </div>
+              <button className="p-2 px-8 text-white bg-shortly-darkViolet rounded-lg hover:opacity-70 focus:outline-none">
+                Copy
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
+            <p className="font-bold text-center text-shortly-veryDarkViolet md:text-left">
+              https://linkedin.com/frontend-mentor
+            </p>
+
+            <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+              <div className="font-bold text-shortly-cyan">
+                https://rel.ink/gob3X9
+              </div>
+              <button className="p-2 px-8 text-white bg-shortly-cyan rounded-lg hover:opacity-70 focus:outline-none">
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
